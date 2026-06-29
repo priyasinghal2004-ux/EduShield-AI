386eccbc2ee9ad873b750721b0a29a88fc8ef6f6
priyasinghal2004-ux <priyasinghal2004@gmail.com>
2026-06-30 02:36:50 +0530

    Remove temporary student seeding

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require('./src/app');
 const env = require('./src/config/env');
 const connectDB = require('./src/config/db');
 const logger = require('./src/utils/logger');
-const seedStudents = require('./seedStudents');
+
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -12,20 +12,15 @@ app.get("/", (req, res) => {
 });
 
 // Connect to MongoDB
-connectDB()
-  .then(async () => {
-    await seedStudents();
+// Connect to MongoDB
+connectDB();
 
-    const server = app.listen(env.PORT, () => {
-      logger.info(`Server running in ${env.NODE_ENV} mode on port ${env.PORT}`);
-    });
+const server = app.listen(env.PORT, () => {
+  logger.info(`Server running in ${env.NODE_ENV} mode on port ${env.PORT}`);
+});
 
-    process.on('unhandledRejection', (err) => {
-      logger.error(`Error: ${err.message}`);
-      server.close(() => process.exit(1));
-    });
-  })
-  .catch((err) => {
-    logger.error(err);
-    process.exit(1);
-  });
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  logger.error(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
